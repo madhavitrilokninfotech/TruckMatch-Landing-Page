97d4bf7c29ec1a277cef10923ae6981574216992
madhavitrilokninfotech <madhavi.mistry@trilokninfotech.com>
2026-07-20 12:22:29 +0530

    header or hero sec responsive done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,12 +11,61 @@ $(window).scroll(function (e) {
     fixedHeader();
 });
 
+
+$('.navbar-nav li a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+});
+
 $(".navbar-toggler").click(function () {
     $("body").toggleClass("no-scroll");
 });
 
 $(".header-menu a").click(function () {
     $("body").removeClass("no-scroll");
+});
+
+$('.header-btn-main a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+});
+
+
+const navLinks = $("#menu-main-menu a");
+navLinks.on("click", function (e) {
+    const fullHref = $(this).attr("href");
+    const targetHash = fullHref.includes("#") ? fullHref.substring(fullHref.indexOf("#")) : null;
+
+    if (targetHash && $(targetHash).length) {
+        e.preventDefault(); // Stop full page reload
+
+        $("html, body").stop(true).animate(
+            { scrollTop: $(targetHash).offset().top - 80 },
+            600
+        );
+
+        navLinks.removeClass("active");
+        $(this).addClass("active");
+    }
+});
+
+// Scroll highlight logic
+$(window).on("scroll", function () {
+    const scrollPosition = $(window).scrollTop();
+
+    navLinks.each(function () {
+        const fullHref = $(this).attr("href");
+        const targetHash = fullHref.includes("#") ? fullHref.substring(fullHref.indexOf("#")) : null;
+        const targetSection = $(targetHash);
+
+        if (targetSection.length) {
+            const offset = targetSection.offset().top - 100;
+            const height = targetSection.outerHeight();
+
+            if (scrollPosition >= offset && scrollPosition < offset + height) {
+                navLinks.removeClass("active");
+                $(this).addClass("active");
+            }
+        }
+    });
 });
 
 
